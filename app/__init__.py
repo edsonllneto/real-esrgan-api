02781d0212fca0da446c7a49b6733149f9629ead
Edson Lopes Lima Neto <edson.llneto@gmail.com>
2025-06-05 18:57:26 -0400

    Add __init__.py to make app a Python package

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Real-ESRGAN API Package
+AI Image Upscaling service using Real-ESRGAN NCNN-Vulkan
+"""
+
+__version__ = "1.0.0"
+__author__ = "Edson Lopes Lima Neto"
+__description__ = "Real-ESRGAN API for image upscaling"
